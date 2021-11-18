f39cc7dc0c436062cf882a99db19fdd78a09bdce
ArturMC <amchojecki@gmail.com>
2021-11-18 18:47:45 -0500

    Created Footer component

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import HeroImage from "../components/HeroImage";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
 import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -50,6 +51,8 @@ export default function Home({ exploreData, cardsData }) {
           buttonText="Get Inspired"
         />
       </main>
+
+      <Footer />
     </div>
   );
 }
