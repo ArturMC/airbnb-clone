d78a6de682e58121274db0ba2f16854ec9061e30
ArturMC <amchojecki@gmail.com>
2021-11-22 21:39:21 -0500

    changed map viewport

--- pages/search.js
+++ pages/search.js
@@ -51,7 +51,7 @@ function Search({ searchResults }) {
           </div>
         </section>
 
-        <section className="hidden xl:inline-flex xl:min-w-[800px] ">
+        <section className="hidden 2xl:inline-flex 2xl:min-w-[800px]">
           <Map searchResults={searchResults} />
         </section>
       </main>
